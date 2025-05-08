ab633af0e624377de30fe98f8fdda9d8c40fb4f0
yaakovm1 <yaakovm1@umbc.edu>
2025-05-08 11:41:19 -0400

    Update dataPlotter.m

--- dataPlotter.m
+++ dataPlotter.m
@@ -1,68 +1,23 @@
-clear; clc; close all;
+x = tiledlayout(2,1);
 
-% Options:
-saveFile = 0; % Enter 1 to save to csv, 0 for off
-
-% Constants:
-NUMPOINTS = 200;
-
-% Find the Arduino serial port
-% If this breaks, open device manager and find the name of the device
-% under ports, then replace dev_name with the name shown
-dev_name = 'Arduino Uno';
-[~,res]=system('wmic path Win32_SerialPort');
-ind = strfind(res,dev_name);
-
-if (~isempty(ind))
-    % If arduino found, save the name and print
-matchLine = extractBetween(res, ind(1), newline);
-tokens = regexp(matchLine{1}, 'COM\d+', 'match');
-port_name = tokens{1};
-
-    fprintf('Arduino is on %s\n',port_name);
-    % Attempt to open the port
-    try
-        % Connect to the port with a 9600 baud rate
-        sro = serialport(port_name,9600);
-        fprintf('%s opened\n',port_name);
-    catch err
-        % If connection fails, print why and exit
-        fprintf('%s\n%s\n',err.identifier,err.message);
-        return
-    end
-else
-    fprintf('COM-port not found\n');
-    return
-end
-
-
-% Flush the serial port
-flush(sro)
-
-% Set up tiled layout
-window = figure;
-x = tiledlayout(3,1);
-
-% Create input position tile
+% Create position tile
+% input position line
 pos_input_ax = nexttile;
-pos_input = animatedline('Marker','o','MarkerSize',3,'MaximumNumPoints',NUMPOINTS+1);
-xlim([0,NUMPOINTS])
-ylim([0,2.5])
-ylabel("Position Input (cm)")
+pos_input = animatedline('Marker','o','MarkerSize',3,'Color','g','MaximumNumPoints',NUMPOINTS+1);
 
-% Create actual position tile
-pos_act_ax = nexttile;
-pos_act = animatedline('Marker','o','MarkerSize',3,'MaximumNumPoints',NUMPOINTS+1);
+% Create actual position line
+pos_act = animatedline('Marker','o','MarkerSize',3,'Color','b', 'MaximumNumPoints',NUMPOINTS+1);
 xlim([0,NUMPOINTS])
-ylim([0,2.5])
-ylabel("Actual Position (cm)")
+ylim([0,3])
+ylabel("Position(cm)")
+legend('Input', 'Actual')
 
 % Create pressure tile
 pressure_ax = nexttile;
 pressure = animatedline('Marker','o','MarkerSize',3,'MaximumNumPoints',NUMPOINTS+1);
 xlim([0,NUMPOINTS])
-ylim([0, 100])
-ylabel("Pressure (kPa)")
+ylim([0, 150])
+ylabel("Pressure (kPa")
 
 % Start counter of the number of readings
 count = 0;
@@ -95,10 +50,10 @@ while ishandle(window) % Run this loop until user closes window
 
     % Update limits
     xlim(pos_input_ax,[max(count-NUMPOINTS,0) max(count,NUMPOINTS)])
-    xlim(pos_act_ax,[max(count-NUMPOINTS,0) max(count,NUMPOINTS)])
     xlim(pressure_ax,[max(count-NUMPOINTS,0) max(count,NUMPOINTS)])
     
     % Draw the plot
     drawnow;
 end
+
 
